9cb6e765a122a3da948a98f937c3fdefe658de27
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-31 11:13:08 +0100

    Server run

--- ChatApp/src/main/java/org/example/App.java
+++ ChatApp/src/main/java/org/example/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         Server server = new Server();
-        Thread serverThread = new Thread(server);
-        serverThread.start();
+        server.run();
     }
 }
