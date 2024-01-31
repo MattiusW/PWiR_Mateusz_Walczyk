70f827cf4161b660c7708f8ab81a55175ded9c30
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-31 11:10:43 +0100

    Server class

--- ChatApp/src/main/java/org/example/App.java
+++ ChatApp/src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Server server = new Server();
+        Thread serverThread = new Thread(server);
+        serverThread.start();
     }
 }
