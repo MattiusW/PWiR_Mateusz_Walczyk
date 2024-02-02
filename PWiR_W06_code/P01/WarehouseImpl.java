a30e2094133e6c6564345b9aec0d53617b1a34f6
Mateusz Walczyk <mattiusw@gmail.com>
2024-02-02 10:45:37 +0100

    Add require funciton

--- PWiR_W06_code/P01/WarehouseImpl.java
+++ PWiR_W06_code/P01/WarehouseImpl.java
@@ -19,4 +19,21 @@ public class WarehouseImpl extends UnicastRemoteObject implements Warehouse {
 
   private Map<String, Double> prices;
 
+  // Add a function that accepts an amount as an argument and returns the name of
+  // the most expensive
+  public String findMaxPricedItemWithinBudget(double budget) throws RemoteException {
+    String maxPricedItem = null;
+    double maxPrice = 0;
+
+    for (Map.Entry<String, Double> entry : prices.entrySet()) {
+      double price = entry.getValue();
+      if (price <= budget && price > maxPrice) {
+        maxPricedItem = entry.getKey();
+        maxPrice = price;
+      }
+    }
+
+    return maxPricedItem;
+  }
+
 }
